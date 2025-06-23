45ef35e927546eb0f591e13fd42ea9fbe09d159b
xyurt <yurtrimu@gmail.com>
2025-06-23 23:35:10 +0300

    Update example_write.c

--- examples/example_write.c
+++ examples/example_write.c
@@ -1,13 +1,13 @@
 #include "yfile.h"
 
 int main() {
-	FILE *fp;							   // File pointer							
-	const char *filename = "example.txt";  // File name
-	char buffer[] = "Hello, World!"; 	   // Buffer which holds the content
+	FILE *fp;		                 // File pointer							
+	const char *filename = "example.txt";    // File name
+	char buffer[] = "Hello, World!";         // Buffer which holds the content
 
-	fp = file_open(filename, "r+b");	   // The file is opened with 'r+b' mode
-	file_write(fp, buffer, strlen(buffer));// The content is being overwritten
+	fp = file_open(filename, "r+b");         // The file is opened with 'r+b' mode
+	file_write(fp, buffer, strlen(buffer));  // The content is being overwritten
 
-	file_close(fp);						   // File is closed
+	file_close(fp);			         // File is closed
 	return 0;
 }
