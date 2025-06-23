fa23e3e9405bd1ea25a80bf65c79324ad1fa3258
xyurt <yurtrimu@gmail.com>
2025-06-23 23:35:42 +0300

    Update example_read.c

--- examples/example_read.c
+++ examples/example_read.c
@@ -1,13 +1,13 @@
 #include "../yfile.h"
 
 int main() {
-	FILE *fp;							   // File pointer							
-	const char *filename = "example.txt";  // File name
-	char buffer[1024];					   // Buffer to read the contents
+	FILE *fp;			        // File pointer							
+	const char *filename = "example.txt";   // File name
+	char buffer[1024];		        // Buffer to read the contents
 
-	fp = file_open(filename, "r");		   // The file is opened with 'r' read-only mode
-	file_read(fp, buffer, sizeof(buffer)); // File is being read into buffer
+	fp = file_open(filename, "r");	        // The file is opened with 'r' read-only mode
+	file_read(fp, buffer, sizeof(buffer));  // File is being read into buffer
 
-	file_close(fp);						   // File is closed
+	file_close(fp);			        // File is closed
 	return 0;
 }
